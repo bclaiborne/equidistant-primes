b33d3d48922f7691cc2f7e73f98188761392c0d9
Ryan Hiebert <ryan@ryanhiebert.com>
2013-04-15 15:18:16 -0700

    Create a couple new possibilities for functions

--- prime.py
+++ prime.py
@@ -1,19 +1,28 @@
 def prime(a):
-    return all(a % x for x in range(2, a//2 + 1))
+	return all(a % x for x in range(2, a//2 + 1))
+
+def prime(num):
+	remainders = (num % x for x in range(2, num // 2 + 1))
+	return all(remainders)
 
 def primesums(x):
-    for z in reversed(range(1, x//2 + 1)):
-        if prime(z) and prime(x-z):
-            yield (z, x-z)
+	for z in reversed(range(1, x//2 + 1)):
+		if prime(z) and prime(x-z):
+			yield (z, x-z)
 
 def primesums(x):
-    return ((z, x-z) for z in reversed(range(1, x//2+1)) if prime(z) and prime(x-z))
+	return ((z, x-z) for z in reversed(range(1, x//2+1)) if prime(z) and prime(x-z))
+
+def primesums(num):
+	midpoint = num // 2 + 1
+	possibles = reversed(range(1, midpoint))
+	return ((z, num - z) for z in possibles if prime(z) and prime(num - z))
 
 def is_prime(a):
-    if all(a%i for i in range(w, a//2+1)):
-        return True
-    else:
-        return False
+	if all(a%i for i in range(w, a//2+1)):
+		return True
+	else:
+		return False
 
 def is_prime(a):
 	for i in range(2, a//2 + 1):
@@ -27,7 +36,6 @@ if __name__ == "__main__":
 		x = int(input('Enter any integer greater than 1: '))
 
 		for z in reversed(range(1, x//2 +1)):
-#			print(z, x-z, type(z), type(x-z))
 			if is_prime(z) and is_prime(x-z):
 				print('The two primes are {0} and {1}'.format(z, x-z))
 		print('Function done')
